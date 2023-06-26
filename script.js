609da3504e49e2ff1f18ff5fd8d80e9862096dc1
Mustakim <m.m.kiron@gmail.com>
2023-06-26 21:21:22 +0600

    Fix indentation on HTML, CSS and JS files

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ const cancel =  document.querySelector('.cancel-icon');
 const navList =  document.querySelector('.pages');
 const container = document.querySelector('.container');
 const main = document.querySelector('.main');
+
 hamburger.addEventListener('click',() => {
-navList.classList.add('active');
-cancel.classList.add('active-icon');
-hamburger.classList.add('disabled');
-container.classList.add('active-color');
-main.classList.add('blur');
-}
-);
+    navList.classList.add('active');
+    cancel.classList.add('active-icon');
+    hamburger.classList.add('disabled');
+    container.classList.add('active-color');
+    main.classList.add('blur');
+});
 
 cancel.addEventListener('click', () => {
     navList.classList.remove('active');
     cancel.classList.remove('active-icon');
     hamburger.classList.remove('disabled');
     main.classList.remove('blur');
-})
+});
