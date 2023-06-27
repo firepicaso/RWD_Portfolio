d4607437a11549d3d86c47c5809ce10b3ba73587
Mustakim <m.m.kiron@gmail.com>
2023-06-27 13:11:42 +0600

    Remove eventlister from navigation

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const hamburger = document.querySelector('.hamburger-icon');
-const cancel = document.querySelector('.cancel-icon');
-const navList = document.querySelector('.pages');
 const container = document.querySelector('.container');
-const main = document.querySelector('.main');
-const portfolio = document.getElementById('portfoliolink');
-const about = document.getElementById('aboutlink');
-const contact = document.getElementById('contactlink');
 const navBar = document.querySelector('.nav');
+const hamburger = document.querySelector('.menu-icon');
+const cancel = document.querySelector('.cancel-icon');
+const main = document.querySelector('.main');
+const navList = document.querySelector('.pages');
+
 hamburger.addEventListener('click', () => {
   navList.classList.add('active-list');
   cancel.classList.add('active-icon');
@@ -18,30 +16,6 @@ hamburger.addEventListener('click', () => {
 });
 
 cancel.addEventListener('click', () => {
-  navList.classList.remove('active-list');
-  cancel.classList.remove('active-icon');
-  hamburger.classList.remove('disabled');
-  main.classList.remove('blur');
-  container.classList.remove('active-container');
-  navBar.classList.remove('active-nav');
-});
-portfolio.addEventListener('click', () => {
-  navList.classList.remove('active-list');
-  cancel.classList.remove('active-icon');
-  hamburger.classList.remove('disabled');
-  main.classList.remove('blur');
-  container.classList.remove('active-container');
-  navBar.classList.remove('active-nav');
-});
-about.addEventListener('click', () => {
-  navList.classList.remove('active-list');
-  cancel.classList.remove('active-icon');
-  hamburger.classList.remove('disabled');
-  main.classList.remove('blur');
-  container.classList.remove('active-container');
-  navBar.classList.remove('active-nav');
-});
-contact.addEventListener('click', () => {
   navList.classList.remove('active-list');
   cancel.classList.remove('active-icon');
   hamburger.classList.remove('disabled');
